9c73186855395972af95521fbd59dda5c78cffe2
yomogi-tyannneru <haruto.matsuda@gmail.com>
2023-01-26 13:25:43 +0900

    first commit

---
+++ stocklocation/src/main/java/com/example/demo/StocklocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocklocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocklocationApplication.class, args);
+	}
+
+}
